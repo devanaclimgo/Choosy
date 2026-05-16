22601dc8880337137832593f341daf21f81dd7a1
Ana <anaclimgo@gmail.com>
2026-05-16 18:39:25 -0300

    feat: add validations for liked status and uniqueness of food_option_id in Vote model

--- backend/app/models/vote.rb
+++ backend/app/models/vote.rb
@@ -2,4 +2,12 @@ class Vote < ApplicationRecord
   belongs_to :room
   belongs_to :player
   belongs_to :food_option
+
+  validates :liked, inclusion: { in: [true, false] }
+
+  validates :food_option_id,
+    uniqueness: {
+      scope: :player_id,
+      message: "already voted"
+    }
 end
