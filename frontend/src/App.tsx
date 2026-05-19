6f55ef242b6cd4f49b083197b7116b13e0561019
Ana <anaclimgo@gmail.com>
2026-05-19 15:48:55 -0300

    feat: add routing for CreateRoom, JoinRoom, Voting, WaitingRoom, and Results pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import CreateRoomPage from "./components/CreateRoom";
+import JoinRoom from "./components/JoinRoom";
+import VotingPage from "./components/VotingContent";
+import WaitingRoomPage from "./components/WaitingRoom";
+import ResultsPage from "./components/Results";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/criar-sala" element={<CreateRoomPage />} />
+      <Route path="/entrar" element={<JoinRoom />} />
+      <Route path="/votar" element={<VotingPage />} />
+      <Route path="/sala-de-espera" element={<WaitingRoomPage />} />
+      <Route path="/resultado" element={<ResultsPage />} />
     </Routes>
   );
 }
