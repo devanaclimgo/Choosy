df85a94cb4b74bad6c68c7648eea4796d1457c05
Ana <anaclimgo@gmail.com>
2026-06-02 17:01:57 -0300

    feat: added restart room route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
           post :start
           get :status
           get :results
+          post "/rooms/:id/restart", to: "rooms#restart"
         end
       end
       resources :votes, only: [:create]
